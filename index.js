9c0836e8bc47c9b20290bc1d0662bb27d97775d4
Emma Alexandria <emma.jellemabutler@gmail.com>
2024-11-16 18:36:43 +0200

    Updates

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
-const plugin = require("tailwindcss/plugin");
+import plugin from "tailwindcss/plugin";
 
-/** @type {import('tailwindcss').Config} */
-module.exports = plugin(function ({ addVariant, e, theme, config }) {
-  addVariant("low-contrast", ({ modifySelectors, separator }) => {
-    modifySelectors(({ className }) => {
-      const lowContrast = e(`low-contrast${separator}${className}`);
-      return `.low-contrast .${lowContrast}`;
-    });
-  }),
-    addVariant("high-contrast", ({ modifySelectors, separator }) => {
+function createAndAddVariant(addVariant, e, name, useClass, mediaQuery = "") {
+  if (useClass) {
+    addVariant(name, ({ modifySelectors, separator }) => {
       modifySelectors(({ className }) => {
-        const highContrast = e(`high-contrast${separator}${className}`);
-        return `.high-contrast .${highContrast}`;
+        const childSelector = e(`${name}${separator}${className}`);
+        return `.${name} .${lowContrast}`;
       });
     });
+  } else {
+    addVariant(name, mediaQuery);
+  }
+}
+
+module.exports = plugin(function ({ addVariant, e, theme, config }) {
+  let useClass = false;
+  let configValue = config("accessibleVariants");
+  if (configValue == "media" || configValue == undefined) {
+    useClass = false;
+  } else if (configValue == "selector") {
+    useClass = true;
+  } else {
+    throw new Error(
+      "Invalid strategy selected for accessibleVariants in TailwindCSS config. Please select either 'selector' or 'media'",
+    );
+  }
+
+  createAndAddVariant(
+    addVariant,
+    e,
+    "low-contrast",
+    useClass,
+    "@media (prefers-contrast: less)",
+  );
+  createAndAddVariant(
+    addVariant,
+    e,
+    "high-contrast",
+    useClass,
+    "@media (prefers-contrast: more)",
+  );
+  createAndAddVariant(
+    addVariant,
+    e,
+    "reduce-motion",
+    useClass,
+    "@media (prefers-reduced-motion)",
+  );
+  createAndAddVariant(
+    addVariant,
+    e,
+    "reduce-transparency",
+    useClass,
+    "@media (prefers-reduced-transparency)",
+  );
 });
